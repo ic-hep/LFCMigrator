56eb1283bdbafb864ca91426ade20cca60d300fd
Daniela Bauer <dbauer@cern.ch>
2018-12-13 18:09:27 +0000

    Fix typo in reload script

--- reload_all.sh
+++ reload_all.sh
@@ -7,7 +7,7 @@ rm -f data.db
 ./dump_util.py load_dfc data/dfc.data
 ./dump_util.py load_lfc data/lfc.data
 ./dump_util.py load_se data/ic.dump UKI-LT2-IC-HEP-disk
-./dump_util.py load_se data/lancs.dump UKI-NORTHGRID-LANC-disk
+./dump_util.py load_se data/lancs.dump UKI-NORTHGRID-LANCS-HEP-disk
 ./dump_util.py load_se data/pic.dump pic-disk
 ./dump_util.py load_se data/qmul.dump UKI-LT2-QMUL2-disk
 ./dump_util.py load_se data/ralpp.dump UKI-SOUTHGRID-RALPP-disk
